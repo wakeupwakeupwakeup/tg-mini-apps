b367525548cb022f58e9a06e6b581d21ae74425c
weukali <solugezero@mail.ru>
2025-06-09 15:27:57 +0300

    Добавлен скрипт для запроса полноэкранного режима с использованием Telegram SDK, изменены отступы в шаблоне компонента.

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
+<script setup lang="ts">
+import { requestFullscreen } from "@telegram-apps/sdk-vue";
+
+// import { init } from "@telegram-apps/sdk-vue";
+
+// tryOnMounted(() => {
+//   try {
+//     init({
+//       acceptCustomStyles: false,
+//     });
+//     const {hash} = useRoute()
+//     const launchParams = new URLSearchParams(hash.slice(1))
+//     if (!launchParams.get("tgWebAppPlatform")) {
+//       return navigateTo("/wrong-environment");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     return navigateTo("/wrong-environment");
+//   }
+// })
+await requestFullscreen();
+</script>
+
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+	<NuxtLayout>
+		<NuxtPage />
+	</NuxtLayout>
 </template>
